dd8ce1590369d61dc89085eea0569442103a29c7
jaerius <rylyn1029@naver.com>
2024-12-02 13:34:20 +0900

    first commit

---
+++ components/Navigation.tsx
@@ -0,0 +1,18 @@
+// 'use client';
+
+// import { Home, Settings, Users, FileText } from 'lucide-react';
+// import { Sidebar, SidebarItem } from '@/components/ui/sidebar';
+
+// export function Navigation() {
+//   return (
+//     <Sidebar>
+//       <SidebarItem icon={Home} title="대시보드" href="/" />
+//       <SidebarItem icon={Users} title="사용자 관리">
+//         <SidebarItem title="사용자 목록" href="/users" />
+//         <SidebarItem title="권한 관리" href="/users/permissions" />
+//       </SidebarItem>
+//       <SidebarItem icon={FileText} title="문서" href="/docs" />
+//       <SidebarItem icon={Settings} title="설정" href="/settings" />
+//     </Sidebar>
+//   );
+// }
